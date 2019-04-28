c5657e809ed27e669dcdf5e30d31135f1492eb8e
pedh <hcn518@gmail.com>
2019-04-28 06:01:12 -0400

    add ch9-3

---
+++ codes/kth_quantiles.py
@@ -0,0 +1,35 @@
+import math
+import random
+
+# Be aware of having a package with the same name
+import selection
+
+
+def kth_quantiles(a, k):
+    if k == 1:
+        return list()
+    n = len(a)
+    if not k % 2:
+        i = n // 2 if n % 2 else n // 2 - 1
+        median = selection.select(a, i + 1)
+        selection.partition(a, median)
+        return kth_quantiles(a[:i], k // 2) + [median] + kth_quantiles(a[i + 1:], k // 2)
+    i = (n * k - n - 1) // (2 * k)
+    j = n - 1 - i
+    left = selection.select(a, i + 1)
+    right = selection.select(a, j + 1)
+    selection.partition(a, left)
+    left_quantiles = kth_quantiles(a[:i], k // 2)
+    selection.partition(a, right)
+    right_quantiles = kth_quantiles(a[j + 1:], k // 2)
+    return left_quantiles + [left, right] + right_quantiles
+
+
+if __name__ == "__main__":
+    n = 99
+    k = 10
+    a = list(range(1, n + 1))
+    random.shuffle(a)
+    print(a)
+    print(kth_quantiles(a, k))
+
--- codes/selection.py
+++ codes/selection.py
@@ -1,30 +1,45 @@
 import random
 
-def partition(a, p, r):
-    x = a[r]
-    i = p - 1
-    for j in range(p, r):
-        if a[j] < x:
+
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        x = a[i]
+        while i > 0 and a[i - 1] > x:
+            a[i] = a[i - 1]
+            i -= 1
+        a[i] = x
+
+
+def partition(a, pivot):
+    i = -1
+    j = 0
+    while j < len(a) - 1:
+        if a[j] == pivot:
+            a[-1], a[j] = a[j], a[-1]
+            continue
+        elif a[j] < pivot:
             i += 1
             a[i], a[j] = a[j], a[i]
-    a[r], a[i + 1] = a[i + 1], a[r]
+        j += 1
+    a[-1], a[i + 1] = a[i + 1], a[-1]
     return i + 1
 
 
-def select_r(a, p, r, i):
-    if p == r:
-        return a[p]
-    q = partition(a, p, r)
-    k = q - p + 1
-    if i == k:
-        return a[q]
-    elif i < k:
-        return select_r(a, p, q - 1, i)
-    return select_r(a, q + 1, r, i - k)
-
-
 def select(a, i):
-    return select_r(a, 0, len(a) - 1, i)
+    if len(a) == 1:
+        return a[0]
+    medians = list()
+    for j in range(0, len(a), 5):
+        group = a[j: j + 5]
+        insertion_sort(group)
+        medians.append(group[(len(group) - 1) // 2])
+    pivot = select(medians, (len(medians) - 1) // 2 + 1)
+    k = partition(a, pivot)
+    if i == k + 1:
+        return pivot
+    if i < k + 1:
+        return select(a[:k], i)
+    return select(a[k + 1:], i - k - 1)
 
 
 if __name__ == "__main__":
