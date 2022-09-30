13ec00b39135b21b6edf8043276ef96bb1209136
pedh <hcn518@gmail.com>
2022-09-30 17:59:54 +0800

    add ch14-1

--- setup.el
+++ setup.el
@@ -2,32 +2,36 @@
 
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((dot . t)))
+ '((dot . t)
+   (latex . t)))
 
-(setq org-confirm-babel-evaluate nil)
-
-(setq org-publish-project-alist
-      '(("html"
-	 :base-directory "."
-	 :base-extension "org"
-	 :publishing-directory "_build/html"
-	 :exclude "README\\.org\\|_build"
-	 :recursive t
-	 :publishing-function org-html-publish-to-html
-	 :author "pedh"
-	 :email "hcn518@gmail.com"
-	 :section-numbers nil
-	 :auto-sitemap t
-	 :sitemap-filename "index.org"
-	 :sitemap-title "CLRS Solutions"
-	 :html-link-up "/CLRS-Solutions"
-	 :html-link-home "/CLRS-Solutions"
-	 :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/org-info.css\" />"
-	 :infojs-opt "path:/js/org-info.js")
-	("static"
-	 :base-directory "."
-	 :base-extension "py\\|c\\|png\\|pdf"
-	 :publishing-directory "_build/html"
-	 :recursive t
-	 :publishing-function org-publish-attachment
-	 )))
+(custom-set-variables
+ '(org-confirm-babel-evaluate nil)
+ '(org-babel-latex-preamble
+   (lambda (_)
+     "\\documentclass[preview]{standalone}"))
+ '(org-publish-project-alist
+   '(("html"
+      :base-directory "."
+      :base-extension "org"
+      :publishing-directory "_build/html"
+      :exclude "README\\.org\\|_build"
+      :recursive t
+      :publishing-function org-html-publish-to-html
+      :author "pedh"
+      :email "hcn518@gmail.com"
+      :section-numbers nil
+      :auto-sitemap t
+      :sitemap-filename "index.org"
+      :sitemap-title "CLRS Solutions"
+      :html-link-up "/CLRS-Solutions"
+      :html-link-home "/CLRS-Solutions"
+      :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/org-info.css\" />"
+      :infojs-opt "path:/js/org-info.js")
+     ("static"
+      :base-directory "."
+      :base-extension "py\\|c\\|png\\|svg\\|pdf"
+      :publishing-directory "_build/html"
+      :recursive t
+      :publishing-function org-publish-attachment
+      ))))
